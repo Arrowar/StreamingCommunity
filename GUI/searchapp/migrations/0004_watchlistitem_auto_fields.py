ee7fe78b3be3ed67810bf87703fcf562df303dbe
mr-brune <49653622+mr-brune@users.noreply.github.com>
2026-02-13 17:37:55 +0000

    fix migration

--- GUI/searchapp/migrations/0004_watchlistitem_auto_fields.py
+++ GUI/searchapp/migrations/0004_watchlistitem_auto_fields.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ("searchapp", "0002_watchlistitem_tmdb_id"),
+        ("searchapp", "0003_watchlistitem_is_movie"),
     ]
 
     operations = [

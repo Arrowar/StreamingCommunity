0a69fc7954d672160204e95065c5979e80447558
Hat3 <hat3.1s1k@gmail.com>
2026-02-28 14:13:23 +0100

    py di esempio che logga il nome file scaricato

---
+++ end.py
@@ -0,0 +1,14 @@
+﻿from pathlib import Path
+import sys
+
+
+def main() -> None:
+    args = sys.argv[1:]
+    output_path = Path(__file__).resolve().with_name("script_args.txt")
+
+    with output_path.open("w", encoding="utf-8") as output_file:
+        output_file.write("\n".join(args))
+
+
+if __name__ == "__main__":
+    main()
